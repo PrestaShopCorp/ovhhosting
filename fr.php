1964c6c08a9075d7125c3b0884727ed0a72d18b8
Thomas Nabord <thomas.nabord@prestashop.com>
2015-03-13 16:38:04 +0100

    First commit

---
+++ fr.php
@@ -0,0 +1,15 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ovhhosting}prestashop>ovhhosting_76616fdade82014543034bff864992f4'] = 'OVH Hosting';
+$_MODULE['<{ovhhosting}prestashop>ovhhosting_7ef8e17583c3ce7e394afece583e1925'] = 'PrestaShop installé et hébergé';
+$_MODULE['<{ovhhosting}prestashop>configure_3e8ac37ecd227615222cbc12e1ba7593'] = 'OVH';
+$_MODULE['<{ovhhosting}prestashop>configure_84d803b569176d76894bfa528461d4fe'] = 'Hébergeur Partenaire Officiel de PrestaShop';
+$_MODULE['<{ovhhosting}prestashop>configure_b4bb85d3a02316265ebffd29421a92a2'] = 'Notre partenaire hébergeur vous propose une offre complète de solutions d\'hébergement Web adaptées à PrestaShop :';
+$_MODULE['<{ovhhosting}prestashop>configure_2064be0cb5876adc2978123b662c9768'] = 'Installation Gratuite';
+$_MODULE['<{ovhhosting}prestashop>configure_6ef52850e24fb867dcfc588ea19314af'] = 'Installation et pré-configuration de la dernière version de PrestaShop';
+$_MODULE['<{ovhhosting}prestashop>configure_467ef0b1ecbc15c33ce11764a03d49be'] = 'Environnement optimisé pour les besoins d’une boutique en ligne';
+$_MODULE['<{ovhhosting}prestashop>configure_df9afb2884e781c25baaeb6911c5b325'] = 'Profitez d’une sauvegarde quotidienne de votre boutique';
+$_MODULE['<{ovhhosting}prestashop>configure_f61336111c3d4fe4521341bde1dfa53f'] = 'Support technique réactif et disponible 24h/24, 7j/7';
+$_MODULE['<{ovhhosting}prestashop>configure_3f2b3fe56ea4d08d543c523aa7457933'] = 'Profitez de l\'offre spéciale Prestashop / OVH';
